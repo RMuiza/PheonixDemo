69dee27ed85f8b37a6a866a129eb7bf7d09d27db
Muiza <raihana@vommex.com>
2020-09-24 15:19:13 +0530

    Header Update

--- src/frontend/header.js
+++ src/frontend/header.js
@@ -6,16 +6,33 @@ import Image from 'react-bootstrap/Image'
 // import nav5 from '../images/nav5.png'
 import dots1 from '../images/dots1.png'
 import wNav from '../images/wNav.png'
+import notification from '../images/notification.png'
+import Dropdown from 'react-bootstrap/Dropdown'
 
 function Header(){
     return(
             <Row style={{width:'1580px',height:'50px',backgroundColor:'black', color:'white', display:'flex'}}>
-                <Col sm={2} >
-                    {/* <Image src={wNav} style={{height:'30px', width:'30px'}}></Image> */}
-                </Col>
-                <Col sm={6} > 
-                    <Image src={dots1} style={{height:'30px', width:'30px',right:'80%'}}></Image>
-                </Col>
+
+                            <Dropdown style={{marginTop:'10px'}}>
+                                <Dropdown.Toggle variant="success" id="dropdown-basic" style={{height:'30px', width:'30px',marginLeft:'10px',marginTop:'10px',marginBottom:'20px'}}>
+                                <Image src={wNav} style={{height:'20px', width:'20px'}}/> 
+                                </Dropdown.Toggle>
+
+                                <Dropdown.Menu style={{backgroundColor:'white',}}>
+                                    <Dropdown.Item href="#/action-3">Course Fee</Dropdown.Item><br /><br />
+                                    <Dropdown.Item href="#/action-3">Payments</Dropdown.Item><br /><br />
+                                    <Dropdown.Item href="#/action-3">Wallet</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+
+                    
+                    <span style={{marginLeft:'10px',marginTop:'10px',marginRight:'1200px'}}>Rs.2000</span>
+                
+                    <Image src={dots1} style={{height:'30px', width:'30px',marginLeft:'10px',marginRight:'25px',marginTop:'10px'}}></Image>
+
+                    <Image src={notification} style={{height:'35px', width:'35px',marginLeft:'10px',marginRight:'50px',marginTop:'10px'}}></Image>
+
+                    <span style={{marginTop:'10px'}}>Logout</span>
             </Row>
     );
 }
